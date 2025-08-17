aec72d12770c2de78a7035e3103a150f624a0ea3
harianchala <harikailash2004@gmail.com>
2025-08-17 11:55:54 +0530

    renamed:    postcss.config.mjs -> postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {}, // ✅ correct for Tailwind v4
-    autoprefixer: {},           // keep autoprefixer too
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
 
